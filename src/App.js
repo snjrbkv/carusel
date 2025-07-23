81a8899dc4ca48d0fc4cabb3a50ce6c95cf9f969
snjrbkv <sanjarbekovodilbek@gmail.com>
2025-07-23 15:24:06 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
 
-function App() {
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "./slider.css";
+
+import images from "./images/images";
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [startIndex, setStartIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const swiperRef = useRef(null);
+
+  const sliderImages = Object.entries(images)
+    .filter(([key]) => /^slider(1[0-3]|[1-9])$/.test(key))
+    .sort(
+      (a, b) =>
+        parseInt(a[0].replace("slider", "")) -
+        parseInt(b[0].replace("slider", ""))
+    );
+
+  const openFullscreen = (index) => {
+    setStartIndex(index);
+    setCurrentIndex(index);
+    setIsOpen(true);
+    setTimeout(() => setIsAnimating(true), 10);
+  };
+
+  const closeFullscreen = () => {
+    setIsAnimating(false);
+    setTimeout(() => setIsOpen(false), 300);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") closeFullscreen();
+    };
+    if (isOpen) document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [isOpen]);
+
+  const handlePrev = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Swiper
+        slidesPerView={"auto"}
+        freeMode={true}
+        modules={[FreeMode]}
+        className="gallerySwiper"
+      >
+        {sliderImages.map(([key, src], index) => (
+          <SwiperSlide key={key} className="custom-slide">
+            <div className="img-container">
+              <img src={src} alt={key} className="custom-image" />
+              <img
+                src={images.fullscreen}
+                alt="fullscreen"
+                className="fullscreen-icon"
+                onClick={() => openFullscreen(index)}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {isOpen && (
+        <div className={`fullscreen-overlay ${isAnimating ? "show" : ""}`}>
+          <button className="close-button" onClick={closeFullscreen}>
+            <span className="close-icon">×</span>
+            <span className="close-text">CLOSE</span>
+          </button>
+
+          <div className="custom-pagination"></div>
+
+          <Swiper
+            ref={swiperRef}
+            initialSlide={startIndex}
+            modules={[Pagination]}
+            className="fullscreenSwiper"
+            pagination={{
+              el: ".custom-pagination",
+              clickable: true,
+              renderBullet: (index, className) =>
+                `<span class="${className} custom-bullet"></span>`,
+            }}
+            onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
+          >
+            {sliderImages.map(([key, src]) => (
+              <SwiperSlide key={`full-${key}`}>
+                <div className="fullscreen-img-wrapper">
+                  <img src={src} alt={key} />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
+          {/* Кастомные стрелки */}
+          {currentIndex > 0 && (
+            <div className="prev-arrow custom-arrow" onClick={handlePrev}>
+              <svg width="48" height="16" viewBox="0 0 48 16" fill="white">
+                <path d="M0 8c0 .573.2445 1.0865.6305 1.451l-.004.002L8 16 4 8l4-8L.6265 6.547l.004.002C.2445 6.9135 0 7.427 0 8z" />
+                <path d="M47.6636904 7.66132609L.6726191 7.33207746c-.3688836-.00258463-.6700181.29435935-.6726027.66324294A.66977174.66977174 0 0 0 0 8.00000028c0 .36889265.2990466.66793922.6679392.66793922.00156 0 .00312-.00000546.0046799-.00001639l46.9910713-.32924863C47.849809 8.33737042 48 8.18612343 48 8.00000028s-.150191-.33737013-.3363096-.33867419z" />
+              </svg>
+            </div>
+          )}
+
+          {currentIndex < sliderImages.length - 1 && (
+            <div className="next-arrow custom-arrow" onClick={handleNext}>
+              <svg width="48" height="16" viewBox="0 0 48 16" fill="white">
+                <path d="M48 8a1.99 1.99 0 0 1-.63 1.451l.004.002L40 16l4-8-4-8 7.373 6.547-.003.002c.386.364.63.878.63 1.451z" />
+                <path d="M.336 7.661l46.991-.329A.668.668 0 0 1 48 8a.668.668 0 0 1-.673.668l-46.99-.33a.339.339 0 0 1 0-.677z" />
+              </svg>
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
---
+++ src/images/images.js
@@ -0,0 +1,33 @@
+import slider1 from "./slider1.avif";
+import slider2 from "./slider2.avif";
+import slider3 from "./slider3.avif";
+import slider4 from "./slider4.avif";
+import slider5 from "./slider5.avif";
+import slider6 from "./slider6.avif";
+import slider7 from "./slider7.avif";
+import slider8 from "./slider8.avif";
+import slider9 from "./slider9.avif";
+import slider10 from "./slider10.avif";
+import slider11 from "./slider11.avif";
+import slider12 from "./slider12.avif";
+import slider13 from "./slider13.avif";
+import fullscreen from "./fullscreen.png";
+
+const images = {
+  slider1,
+  slider2,
+  slider3,
+  slider4,
+  slider5,
+  slider6,
+  slider7,
+  slider8,
+  slider9,
+  slider10,
+  slider11,
+  slider12,
+  slider13,
+  fullscreen,
+};
+
+export default images;
